26ecb56ed782c298c2ec1d870525bc511fc36844
Danny Díaz <meliodasfistery@gmail.com>
2021-07-09 17:42:57 -0500

    fix favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Fragment } from 'react'
+
+const prefix = "/Portfolio"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <link rel="shortcut icon" href={prefix+'/favicon.ico'} />
+      <Component {...pageProps} />
+    </Fragment>
+  );
+  
+  
 }
 export default MyApp
