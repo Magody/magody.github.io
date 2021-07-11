dbff62e3cd114881c2ca9dccf5be8eb05ee5b229
Danny Díaz <meliodasfistery@gmail.com>
2021-07-10 23:06:48 -0500

    fix base path to empty

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withTM = require('next-transpile-modules')(['three']);
 
 module.exports = withTM({
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   // ...nextTranslate()
 })
