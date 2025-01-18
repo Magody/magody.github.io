2246b80cfef73adb540cbb55df2f7a2a04c9f191
Danny Diaz <meliodasfistery@gmail.com>
2025-01-17 22:13:10 -0500

    reverse deploy script

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true, // Para asegurar que las rutas terminen con '/'
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/portfolio', // Esto permite personalizar el subdirectorio /portfolio
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '', // Esto permite personalizar el subdirectorio /portfolio
   images: {
     unoptimized: true, // Necesario para exportar imágenes sin optimización
   },
